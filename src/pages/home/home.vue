ede9ccde54948c9bebabfe78e74b9ef6839f7d12
longdan <1060921782@qq.com>
2018-06-28 11:18:01 +0800

    项目初始化

---
+++ src/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+  home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'heme'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
