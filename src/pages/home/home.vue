8db5de516af5af016649f9f04911487c36655fc2
longdan <1060921782@qq.com>
2018-06-28 17:48:17 +0800

    完成顶部和轮播图

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="hello">
-  home
-  </div>
+<div class="hello">
+	<home-header></home-header>
+	<home-swiper></home-swiper>
+</div>
 </template>
 
 <script>
+import homeHeader from './components/header.vue'
+import homeSwiper from './components/swiper.vue'
 export default {
-  name: 'heme'
+  name: 'home',
+  components: {homeHeader,homeSwiper}
 }
 </script>
 
